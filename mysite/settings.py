a21de0de54d0c249ab9ca84e89a945621cc2d51c
Michael <michaelnz10@outlook.com>
2022-11-26 18:20:46 +1300

    added allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoasgi.spinningcube.co.nz', 'purrycat.tk']
 
 
 # Application definition
