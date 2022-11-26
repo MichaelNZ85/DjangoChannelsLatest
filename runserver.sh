5de4e756b99184965f2f294bd26f0fb2573f4c83
Michael <michaelnz10@outlook.com>
2022-11-26 18:19:07 +1300

    tweeked runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python manage.py collectstatic --no-input && python manage.py migrate && daphne mysite.asgi:application -b 0.0.0.0:80
+python manage.py collectstatic --no-input && python manage.py migrate && daphne mysite.asgi:application -b 0.0.0.0 -p 80
